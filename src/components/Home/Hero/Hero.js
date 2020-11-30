69af62368c8a50485c824faf71c126ad113c8682
Jorge <jhessle04@gmail.com>
2020-11-30 15:36:23 -0300

    U

--- src/components/Home/Hero/Hero.js
+++ src/components/Home/Hero/Hero.js
@@ -7,16 +7,12 @@ const Hero = () => {
     return (
         <div className='hero' id='hero-navigate'>
             <div className='hero-text'>
-                <h1 className='hero-h1'>We Make Amazing Haircuts</h1>
+                <h1 className='hero-h1'>Los mejores cortes</h1>
                 <div className='hero-para-div'>
                     <p className='hero-para'>
                     Desde 2015 nos dedicamos a mejorar la imagen de nuestros clientes.
 
-                   Construimos un espacio en el mundo para que disfrutes y te sientas cómodo.
-
-                   Queremos que Barbería Shop sea tu lugar donde, más allá de cambiar tu look,
-
-                   puedas relajarte, disfrutar de una buena cerveza y encontrarte con amigos.
+                   Construimos un espacio en el mundo para que disfrutes.
                     </p>
                 </div>
                 <Link to='/appointment'>
